db71bb2acc7f8bef902c552f254e83c8da2971f7
Ridwan Setiadi <ridwansetiadi94@gmail.com>
2025-11-24 21:28:20 +0700

    adding billboard env

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,13 @@ export const revalidate = 0;
 
 async function Homepage() {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("5dbfcaa6-4edb-4a48-85b2-8f6790bc1dc5");
+  const billboardId = process.env.NEXT_PUBLIC_BILLBOARDID;
+  if (!billboardId) {
+    throw new Error(
+      "NEXT_PUBLIC_BILLBOARDID environment variable is not defined"
+    );
+  }
+  const billboard = await getBillboard(billboardId);
   return (
     <Container>
       <div className="space-y-10 pb-10">
