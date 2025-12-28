aceecf9ee20f9a63d3e4c4f3ec05f877f8eb0b31
Ridwan Setiadi <ridwansetiadi94@gmail.com>
2025-12-28 12:03:52 +0700

    added new error handling if fetching billboard fails

--- actions/get-billboards.tsx
+++ actions/get-billboards.tsx
@@ -4,6 +4,11 @@ const billboardURL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
 async function getBillboard(id: string): Promise<Billboard> {
   const res = await fetch(`${billboardURL}/${id}`);
+
+  if (!res.ok) {
+    throw new Error(`Billboard fetch failed: ${res.status}`);
+  }
+
   return res.json();
 }
 
